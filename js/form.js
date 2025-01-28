14176ad606aaa4dd40de4154f8bcb7ff01f1ec31
Franklimello <frank.melo.wal@gmail.com>
2025-01-28 20:53:51 -0300

    atualizacao

--- js/form.js
+++ js/form.js
@@ -5,7 +5,8 @@ const altura = pegaElemento('#ialtura');
 const resultado = pegaElemento('#imsg');
 const btn_calc = pegaElemento('.btn_calc');
 
-
+preenchimento('#ialtura','altura')
+preenchimento('#ipeso','peso')
 
 
 btn_calc.addEventListener('click', (event) => {
@@ -19,7 +20,7 @@ btn_calc.addEventListener('click', (event) => {
     let altura_valor = parseFloat(altura.value); 
 
 
-    if (!verificarPreenchimento){
+    if (!verificarPreenchimento(nome,idade,peso,altura)){
         resultado.style.color = 'red';
         resultado.value = 'Por favor preencha todos os campos'
     }else if(!naoNumero(peso_valor,altura_valor)) {
@@ -27,8 +28,21 @@ btn_calc.addEventListener('click', (event) => {
         resultado.value = 'Por favor preencha todos os campos'
     }else {
         let imc = calculaImc(peso,altura);
-        resultado.style.color = 'black'
-        resultado.value = exibirResultado(imc, peso, altura,nome);
+        if (imc < 18.5){
+            resultado.style.color = 'black'
+            resultado.innerHTML = exibirResultado(imc, peso, altura,nome)+exibiMsg(' Voce esta abaixo do peso'); 
+        }else if (imc >= 18.5 && imc <= 24.9){
+            resultado.innerHTML = exibirResultado(imc, peso, altura,nome)+ 'Seu peso esta normal'
+        }else if (imc >=25.0 && imc <= 29.9){
+            resultado.innerHTML = exibirResultado(imc, peso, altura,nome)+exibiMsg(' Voce esta com sobrepeso');
+        }else if (imc >= 30 && imc <= 34.9){
+            resultado.innerHTML = exibirResultado(imc, peso, altura,nome)+exibiMsg(' voce tem obesidade grau 1');
+        }else if (imc >= 35.0 && imc <= 39.9){
+            resultado.innerHTML = exibirResultado(imc, peso, altura,nome)+exibiMsg(' voce esta com obesidade grau 2');
+        }else {
+            resultado.innerHTML = exibirResultado(imc, peso, altura,nome)+exibiMsg(' voce esta com obesidade grau 3 (mórbida)');
+        }
+        
     }
      
 })
@@ -64,7 +78,7 @@ function calculaImc (peso,altura){
 }
 
 function exibirResultado(imc, peso, altura,nome){
-    return `Sua altura e (${altura.value}M) e seu peso e (${peso.value}kg) e seu IMC e de (${imc}), obrigado por usar o programa ${nome.value}`
+    return `Sua altura e (${altura.value}M) e seu peso e (${peso.value}kg) e seu IMC e de (${imc}), obrigado por usar o programa ${nome.value}! `
 }
 
 function verificarPreenchimento(nome,idade,peso,altura){
@@ -87,3 +101,26 @@ function converterDados(altura, peso) {
         peso: pesoFormatado
     };
 }
+
+function exibiMsg(msg) {
+    return `<span style="color: red;">${msg}</span>`;
+}
+
+function preenchimento(elemento, tipo) {
+    const input = pegaElemento(elemento);
+    input.addEventListener('input', (event) => {
+        let valor = event.target.value.replace('.', '');
+        if (tipo === 'altura' && valor.length >= 2) {
+            valor = valor.substring(0, 1) + '.' + valor.substring(1);
+        } else if (tipo === 'peso') {
+            if (valor.length >= 4) {
+                valor = valor.substring(0, 3) + '.' + valor.substring(3);
+            } else if (valor.length >= 3) {
+                valor = valor.substring(0, 2) + '.' + valor.substring(2);
+            }
+        }
+        event.target.value = valor;
+    });
+}
+
+
