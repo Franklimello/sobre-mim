c4ea3aeb2de132f188d5e375b3610f023f136a2c
Franklimello <frank.melo.wal@gmail.com>
2025-01-28 15:44:27 -0300

    atualizacao

---
+++ js/form.js
@@ -0,0 +1,68 @@
+const nome = pegaElemento('#inome');
+const idade = pegaElemento('#iidade');
+const peso = pegaElemento('#ipeso');
+const altura = pegaElemento('#ialtura');
+const resultado = pegaElemento('#imsg');
+const btn_calc = pegaElemento('.btn_calc');
+
+
+btn_calc.addEventListener('click', (event) => {
+    event.preventDefault();
+    let peso_valor = parseFloat(peso.value);
+    let altura_valor = parseFloat(altura.value); 
+
+
+    if (!verificarPreenchimento){
+        resultado.style.color = 'red';
+        resultado.value = 'Por favor preencha todos os campos'
+    }else if(!naoNumero(peso_valor,altura_valor)) {
+        resultado.style.color = 'red';
+        resultado.value = 'Por favor preencha todos os campos'
+    }else {
+        let imc = calculaImc(peso,altura);
+        resultado.value = exibirResultado(imc, peso, altura,nome);
+        resultado.value.color = ''
+    }
+     
+})
+
+
+
+function pegaElemento (elemento){
+    const el = document.querySelector(elemento);
+    if (el) {
+        return el;
+    }else {
+        console.error(`ERRO! Verifique o seletor ${elemento}`)
+        return null;
+    }
+       
+}
+
+function naoNumero (... numeros) {
+    for (numero of numeros){
+        if(isNaN(numero)){
+            return false;
+        }
+    }
+    return true
+}
+
+function calculaImc (peso,altura){
+    let peso_valor = parseFloat(peso.value);
+    let altura_valor = parseFloat(altura.value);
+    naoNumero(peso_valor,altura_valor)
+    let imc = peso_valor/(altura_valor*altura_valor)
+    return imc.toFixed(2);
+}
+
+function exibirResultado(imc, peso, altura,nome){
+    return `Sua altura e (${altura.value}M) e seu peso e (${peso.value}kg) e seu IMC e de (${imc}), obrigado por usar o programa ${nome.value}`
+}
+
+function verificarPreenchimento(nome,idade,peso,altura){
+    if(nome.value == '' || idade.value == '' || altura.value == '' ||peso.value == ''){
+        return false
+    }
+    return true
+}
